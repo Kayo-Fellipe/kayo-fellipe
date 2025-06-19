b6ad807b2ac97aa61b839211e84a33bc6eeb0c77
Kayo Fellipe <kayofellipefer@gmail.com>
2025-06-19 14:05:07 -0300

    Up

--- js/portfolio.js
+++ js/portfolio.js
@@ -110,7 +110,7 @@ const portfolioData = {
     items: [
       {
         type: "youtube",
-        src: "G10nMDgvtrPHAqnN",
+        src: "Z9ncFlpvXrXJaMQA",
         thumbnail: "https://i.postimg.cc/D0Wp043k/IMG-4821.jpg"
       },
     ]
